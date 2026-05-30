312d87921f78e6a47bdc2237c6294bc084b2f77c
aguer47 <maguer@byupathway.edu>
2026-05-30 14:16:18 +0300

    add changes

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get("/user", (req, res) => {
   if (req.isAuthenticated()) {
     res.json(req.user);
   } else {
-    res.status(401).json({ message: "Not authenticated" });
+    res.status(401).send("Not authenticated");
   }
 });
 
